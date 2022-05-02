9e50033388914c9ac19522ea2415d351718f75ca
adarshnd14 <adarsh14@gmail.com>
2022-05-02 09:13:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
+import React, { useContext } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
+import Navigation from './components/navigation/Navigation';
+import Home from './components/body/Home';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Footer from './components/footer/Footer';
+import MenBody from './components/Men.js/MenBody';
+import WomenBody from './components/women/WomenBody';
+import KidsBody from './components/kids/KidsBody';
+import HomeAndLiving from './components/homeAndLiving/HomeAndLiving';
+import LogIn from './components/login/LogIn';
+import WishList from './components/wishlist/WishList';
+import FilterMen from './components/filter/FilterMen';
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+
+        <Navigation />
+        <Route exact path={'/'} component={Home} />
+        <Route path={'/men'} component={MenBody} />
+        <Route path={'/women'} component={WomenBody} />
+        <Route path={'/kids'} component={KidsBody} />
+        <Route path={'/homeandliving'} component={HomeAndLiving} />
+        <Route path={'/login'} component={LogIn} />
+        <Route path={'/wishlist'} component={WishList} />
+        <Route path={'/filtermen'} component={FilterMen} />
+
+        <Footer />
+        <hr />
+      </div>
+    </Router>
   );
 }
 
